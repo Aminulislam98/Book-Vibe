4be7cbf672321b056a0c456cd471dedbb14961d8
Aminul islam <aminurrohman98@gmail.com>
2026-04-05 15:49:10 +0100

    added router , createBrowserRouter, added component and  layout component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  RouterContextProvider,
+  RouterProvider,
+} from "react-router";
+import MainLayout from "./MainLayout/MainLayout.jsx";
+import HomePage from "./Pages/HomePage/HomePage.jsx";
+import BookPage from "./Pages/HomePage/Book/BookPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: "/bookPage",
+        Component: BookPage,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
-)
+);
